014aa8b92ea21a3b78980438e5fb3defcfe23d6f
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-05-28 16:57:18 +0530

    add sqs

--- ll/lambda.js
+++ ll/lambda.js
@@ -1,20 +1,25 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-    sns.publish({
-        Message: 'Test',
-        MessageAttributes: {
-            'AWS.SNS.SMS.SMSType': {
-                DataType: 'String',
-                StringValue: 'Promotional'
-            },
-        },
-        PhoneNumber: '+940772445224'
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/KTestSQS`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
     }).promise()
-        .then(data => {
-            // your code goes here
-            console.log(data);
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
         })
         .catch(err => {
             // error handling goes here
